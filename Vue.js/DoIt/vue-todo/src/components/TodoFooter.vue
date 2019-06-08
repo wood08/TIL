efa1ee2ec5931e7bc62b4c85a14e5e02388b902b
wood08 <wood08@hotmail.com>
2019-06-08 21:38:37 +0900

    Do it! Vue.js 입문 실습 chapter_06 - 리스트와 전체삭제 버튼 css 추가

--- Vue.js/DoIt/vue-todo/src/components/TodoFooter.vue
+++ Vue.js/DoIt/vue-todo/src/components/TodoFooter.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <button v-on:click="clearAll">Clear All</button>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" v-on:click="clearAll">Clear All</span>
     </div>
 </template>
 
@@ -15,5 +15,16 @@
 </script>
 
 <style>
-
+    .clearAllContainer {
+        width: 8.5rem;
+        height: 50px;
+        line-height: 50px;
+        background-color: white;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+    .clearAllBtn {
+        color: #e20303;
+        displa: black;
+    }
 </style>
--- Vue.js/DoIt/vue-todo/src/components/TodoList.vue
+++ Vue.js/DoIt/vue-todo/src/components/TodoList.vue
@@ -2,9 +2,10 @@
     <section>
         <ul>
             <li v-for="(todoItem, index) in todoItems">
+                <i class="checkBtn fas fa-check" aria-hidden></i>
                 {{ todoItem }}
-                <span class="removeContainer" v-on:click="removeTodo(todoItem, index)">
-                    <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
                 </span>
             </li>
         </ul>
@@ -23,5 +24,29 @@
 </script>
 
 <style>
-
+    ul {
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn {
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
